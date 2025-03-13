9ee03d7cda9459f5cd0eaef4965c6b139615d095
Cameron Black <cameron.black@stvincent.edu>
2025-03-12 23:14:42 -0400

    updated user interface with some functionality, allows the user to create list and items as well as change the quantity for each item with + and -

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,228 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(InventoryApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class InventoryApp extends StatelessWidget {
+  const InventoryApp({super.key});
 
-  // This widget is the root of your application.
+  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Inventory App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+
+        scaffoldBackgroundColor: const Color.fromARGB(255, 26, 26, 26),
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.grey,
+        ).copyWith(
+          secondary: Colors.white,
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ListScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ListScreen extends StatefulWidget {
+  const ListScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ListScreenState createState() => _ListScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ListScreenState extends State<ListScreen> {
+  List<String> lists = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _createNewList() {
+    TextEditingController controller = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(
+          'Create New List',
+          style: GoogleFonts.montserrat(),
+        ),
+        content: TextField(controller: controller, decoration: InputDecoration(hintText: 'List Name'), style: GoogleFonts.montserrat()),
+        
+        actions: [
+          TextButton(
+            onPressed: () {
+              if (controller.text.isNotEmpty) {
+                setState(() => lists.add(controller.text));
+                Navigator.pop(context);
+              }
+            },
+            child: Text('Add', style: GoogleFonts.montserrat()),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+
+        child: AppBar(
+          title: Text(
+            '\nYour Lists',
+            style: GoogleFonts.montserrat(
+              textStyle: TextStyle(
+                color: Colors.white,
+                fontSize: 20, 
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ],
+          ),
+          backgroundColor: const Color.fromARGB(255, 26, 26, 26)
+        ),
+      ),
+
+      body: ListView.builder(
+        itemCount: lists.length,          
+        itemBuilder: (context, index) => Card(
+          color: Colors.blueGrey,
+          margin: EdgeInsets.all(8),
+          child: ListTile(
+            title: Text(
+              lists[index],
+              style: GoogleFonts.montserrat(
+                textStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ItemScreen(listName: lists[index]),
+              ),
+            ),
+          )
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _createNewList,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class ItemScreen extends StatefulWidget {
+  final String listName;
+  const ItemScreen({super.key, required this.listName});
+
+  @override
+  _ItemScreenState createState() => _ItemScreenState();
+}
+
+class _ItemScreenState extends State<ItemScreen> {
+  List<Map<String, dynamic>> items = [];
+
+  void _addItem() {
+    TextEditingController itemController = TextEditingController();
+    TextEditingController quantityController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Add Item', style: GoogleFonts.montserrat()),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(controller: itemController, decoration: InputDecoration(hintText: 'Item Name'), style: GoogleFonts.montserrat()),
+            TextField(controller: quantityController, decoration: InputDecoration(hintText: 'Quantity'), style: GoogleFonts.montserrat(), keyboardType: TextInputType.number),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              if (itemController.text.isNotEmpty && quantityController.text.isNotEmpty) {
+                setState(() => items.add({'name': itemController.text, 'quantity': int.parse(quantityController.text)}));
+                Navigator.pop(context);
+              }
+            },
+            child: Text('Add', style: GoogleFonts.montserrat()),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _updateQuantity(int index, int change) {
+    setState(() => items[index]['quantity'] = (items[index]['quantity'] + change).clamp(0, 999));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.listName,
+            style: GoogleFonts.montserrat(
+              textStyle: TextStyle(
+                color: Colors.white,
+                fontSize: 20, 
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          backgroundColor: const Color.fromARGB(255, 26, 26, 26),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () => Navigator.pop(context),
+          ),
+      ),
+    
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) => Card(
+          color: Colors.blueGrey,
+          margin: EdgeInsets.all(8),
+          child: ListTile(
+            title: Text(
+              items[index]['name'],
+              style: GoogleFonts.montserrat(
+                textStyle: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            
+            subtitle: Text(
+              'Quantity: ${items[index]['quantity']}',
+              style: GoogleFonts.montserrat(
+                textStyle: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(icon: Icon(Icons.remove, color: Colors.white), onPressed: () => _updateQuantity(index, -1)),
+                IconButton(icon: Icon(Icons.add, color: Colors.white), onPressed: () => _updateQuantity(index, 1)),
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addItem,
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
