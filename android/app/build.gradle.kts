0a71ccabdca57cee5826a5e9b615ece277e31c90
Isaac Samsel <isaac.samsel@stvincent.edu>
2025-04-01 10:59:21 -0400

    Firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,18 +3,24 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.jjbic.inventorymanagement"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
 
+    dependencies {
+        implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
+
+        implementation("com.google.firebase:firebase-analytics")
+    }
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
