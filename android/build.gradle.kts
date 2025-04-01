8a3188854344625564cab453ba026aba18aabb1e
Isaac Samsel <isaac.samsel@stvincent.edu>
2025-04-01 12:52:54 -0400

    Firebase Code Implemenation

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,9 @@
+buildscript{
+    dependencies{
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
 plugins {
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
