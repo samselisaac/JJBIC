6e6110bbdf3bf9d15c0b9dd176cf532c2ff4ac1c
Isaac Samsel <isaac.samsel@stvincent.edu>
2025-04-09 01:14:55 -0400

    Firebase Emulator

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,19 @@ import 'package:flutter/material.dart';
 import 'screens/lists_overview_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 //import 'auth_service.dart';
 
 FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> firebase
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  
   runApp(InventoryApp());
 }
 
