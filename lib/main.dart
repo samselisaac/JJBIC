dc958db300c5a725b0e9f4f568584bc6542f3bb2
Isaac Samsel <isaac.samsel@stvincent.edu>
2025-04-08 23:17:14 -0400

    Analytics Additions

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 
 FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
